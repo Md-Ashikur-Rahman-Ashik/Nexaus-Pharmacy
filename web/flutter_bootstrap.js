164319c696d2aaa295bb940ad39a6736a7bfd02f
Cirrus CI <support@cirruslabs.org>
2026-06-23 04:04:00 +0000

    Fixed output issue

--- web/flutter_bootstrap.js
+++ web/flutter_bootstrap.js
@@ -1,19 +1,3 @@
 {{flutter_js}}
 {{flutter_build_config}}
-
-_flutter.loader.load({
-  onEntrypointLoaded: async function(engineInitializer) {
-    let appRunner = async function() {
-      let engine = await engineInitializer.initializeEngine({
-        renderer: "html"
-      });
-      await appRunnerEngine(app);
-    };
-    if (window._flutter) {
-      window._flutter.appRunner = appRunner;
-    } else {
-      window._flutter = { appRunner: appRunner };
-      appRunner();
-    }
-  }
-});
+_flutter.loader.load();
