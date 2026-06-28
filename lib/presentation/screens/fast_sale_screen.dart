97bfd035d26359341e52b2713964c648e1c2cf1e
Cirrus CI <support@cirruslabs.org>
2026-06-28 00:44:30 +0000

    Added missing import

--- lib/presentation/screens/fast_sale_screen.dart
+++ lib/presentation/screens/fast_sale_screen.dart
@@ -1,3 +1,4 @@
+import 'package:pharmacy_app/presentation/providers/sale_provider.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:pharmacy_app/presentation/providers/sale_provider.dart';
